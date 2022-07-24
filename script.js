627a5f7369a2abf20fe503c68fa3eebd675d4b55
FreakyRickyy <rickyjustwork@gmail.com>
2022-07-24 17:58:25 +0530

    rickyswork website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('signin-btn').addEventListener('click', function () {
+    document.querySelector('.signin').style.display = 'flex';
+});
+
+document.getElementById('close-signin').addEventListener('click', function () {
+    document.querySelector('.signin').style.display = 'none';
+});
