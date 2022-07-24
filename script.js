14b21a803f0e1bfa79e60f316172767bfd692c36
FreakyRickyy <109904050+FreakyRickyy@users.noreply.github.com>
2022-07-24 18:19:15 +0530

    yup

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('signin-btn').addEventListener('click', function () {
-    document.querySelector('.signin').style.display = 'flex';
-});
-
-document.getElementById('close-signin').addEventListener('click', function () {
-    document.querySelector('.signin').style.display = 'none';
-});
